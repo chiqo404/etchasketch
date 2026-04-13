19c8be767012d2c863d6c06aea7639a0e6be6ca2
chiqo404 <270429796+chiqo404@users.noreply.github.com>
2026-04-13 21:12:14 +0400

    etch a sketch

--- jscript.js
+++ jscript.js
@@ -1,6 +1,61 @@
 const container = document.querySelector("#container");
-for(let i = 0; i < 256; i++){
-    const square = document.createElement("div");
-    square.classList.add("square");
-    container.appendChild(square);
+function createGrid(size){
+    for(let i = 0; i < size*size; i++){
+        const square = document.createElement("div");
+        square.classList.add("square");
+                square.style.width = `calc(100%/${size})`;
+        container.appendChild(square);
+    }
 }
+function deleteGrid(size){
+    for(let i = 0; i < size*size; i++){
+        const square = document.querySelector(".square");
+        square.remove();
+    }
+}
+
+
+
+let size = 16;
+createGrid(size);
+const rgb = document.querySelector("#rgb");
+const b = document.querySelector("#black");
+const e = document.querySelector("#eraser");
+let k = 0;
+rgb.addEventListener("click", () => {k=1});
+b.addEventListener("click", () => {k=0});
+e.addEventListener("click", () => {k=2});
+const squares = document.querySelectorAll('.square');
+let ind = false;
+window.addEventListener("mousedown", () => ind = true);
+window.addEventListener("mouseup", () => ind = false);
+squares.forEach((square) => {
+    square.addEventListener("mouseover", () => {
+        if(ind == true){
+        if(k == 0) square.style.backgroundColor = "black";
+        else if(k == 1) square.style.backgroundColor =  `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+        else square.style.backgroundColor="white";
+        }
+    });
+});
+
+const btn = document.querySelector("button");
+ btn.addEventListener("click", () =>{
+    deleteGrid(size);
+    size = prompt("pick your grid size");
+    if(size > 100) size = 100;
+    createGrid(size);
+    const squares = document.querySelectorAll('.square');
+    let k = 0;
+    squares.forEach((square) => {
+        square.addEventListener("mouseover", () => {
+            if(ind == true){
+        if(k == 0) square.style.backgroundColor = "black";
+        else if(k == 1) square.style.backgroundColor =  `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+        else square.style.backgroundColor="white";
+        }
+    });
+ });
+ 
+});
+
